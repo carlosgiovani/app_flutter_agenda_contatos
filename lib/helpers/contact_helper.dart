0574a34bc796afddccb18caf14e7a296f28a0338
Carlos Giovani Casilo <carlos.giovani@implantainformatica.com.br>
2019-09-11 16:42:12 -0300

    App para armazenar contatos

---
+++ lib/helpers/contact_helper.dart
@@ -0,0 +1,143 @@
+import 'dart:async';
+import 'dart:core' as prefix0;
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+// Nome das colunas do banco
+final String contactTable = "contactTable";
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+final String imgColumn = "imgColumn";
+
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  // Declaração do banco
+  Database _db;
+
+  // inicialização do BD
+  Future<Database> get db async {
+    if(_db != null){
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  //Função para iniciar o DB - asyn e await prq vai esperar retornar o caminho
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath(); //pega o caminho do db
+    final path = join(databasesPath, "contacts.db"); //pega o caminho e junta com o nome do banco
+
+    //Cria tabela no db
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)"
+      );
+    });
+  }
+
+  // Função para salvar os contatos
+  Future<Contact> saveContact(Contact, contact) async{
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  // Função para obter os dados do contato
+  Future<Contact> getContact (int id) async{
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+    columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+    where: "$idColumn = ?",
+    whereArgs: [id]);
+
+    if(maps.length > 0){
+      return Contact.fromMap(maps.first);
+    } else{
+      return null;
+    }
+  }
+
+  //Função para deletar um contato
+  Future<int> deleteContact(int id) async{
+    Database dbContact = await db;
+    return await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
+  }
+
+  // Função para atualizar contato
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+    return await dbContact.update(contactTable, contact.toMap(), where: "$idColumn = ?", whereArgs: [contact.id]);
+  }
+
+  //Função para obter todos os contatos
+  Future<List> getAllContact() async {
+    Database dbContact = await db;
+    List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
+    List<Contact> listContact = List();
+    for(Map m in listMap) {
+      listContact.add(Contact.fromMap(m));
+    }
+  }
+
+  //Função para verificar a quantidade de registro
+  Future<int> getNumber() async{
+    Database dbContact = await db;
+    return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
+  }
+
+  //Função para fechar o banco
+  Future close() async{
+    Database dbContact = await db;
+    dbContact.close();
+  }
+}
+
+class Contact {
+  int id;
+  String name;
+  String email;
+  String phone;
+  String img;
+
+  // Construtor - converte para um MAP
+  Contact.fromMap(Map map){
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+  }
+
+  // Converte de Map para String
+  Map toMap(){
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: img
+    };
+    if(id != null){
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)";
+  }
+
+
+}
